b98da5152e8ffb9a1eb326325de9532b389bb8f2
Nate V <nate.tripblazr@gmail.com>
2019-08-27 21:20:18 -0500

    added cookout

--- main.js
+++ main.js
@@ -34,3 +34,53 @@ console.log(oar);
 
 const artic = takeNumber("Arctic Donkeys");
 console.log(artic);
+
+// cookout
+
+const hamburger = {
+	name: "Hamburger",
+	type: "beef",
+	cooked: false
+};
+const zucchini = {
+	name: "Zucchini",
+	type: "vegetable",
+	cooked: false
+};
+const chickenBreast = {
+	name: "Chicken Breast",
+	type: "chicken",
+	cooked: false
+};
+const corn = {
+	name: "Corn",
+	type: "vegetable",
+	cooked: false
+};
+const steak = {
+	name: "Steak",
+	type: "beef",
+	cooked: false
+};
+
+// An array that is grouping the objects together.
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+console.log("cooked food status:", foods[0].name, foods[0].cooked);
+console.log("cooked food status:", foods[1].name, foods[1].cooked);
+
+function grill(currentObject) {
+	currentObject.cooked = true;
+	cookedFood.push(currentObject);
+}
+
+foods.forEach(element => {
+	grill(element);
+});
+
+console.log("cooked food", cookedFood);
+console.log("cooked food status:", foods[0].name, foods[0].cooked);
+console.log("cooked food status:", foods[1].name, foods[1].cooked);
